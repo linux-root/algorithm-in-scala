665cec9ba37de95bc49483a359e49261ee9b8221
Watson Dinh <mr.kurro@gmail.com>
2025-04-07 20:43:01 -0500

    add flake.nix

---
+++ red-black-tree-visualization/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Redblack tree visualization";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    sbt-derivation.url = "github:zaninime/sbt-derivation";
+    sbt-derivation.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, sbt-derivation }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = sbt-derivation.mkSbtDerivation.${system} {
+          pname = "rbtv";
+          version = "0.1.0";
+          src = ./.;
+
+          # Hash for the dependencies - will fail the first time with the correct hash
+          # Replace with the hash from the error message
+          depsSha256 = "sha256-change=";
+          
+          # SBT command to build the site
+          buildPhase = ''
+            sbt pd
+          '';
+
+          # Copy the built site to the output
+          installPhase = ''
+            mv dist $out
+          '';
+        };
+
+        # Make the package available as a NixOS module option
+        nixosModule = { config, lib, pkgs, ... }: {
+          options.rbtv-package = lib.mkOption {
+            type = lib.types.package;
+            description = "redblack tree visualization";
+            default = self.packages.${pkgs.system}.default;
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            sbt
+            jdk
+          ];
+        };
+      });
+}
